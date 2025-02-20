755b7f673fb67996beb9f959871adda4144c6839
rodgar00 <rodrigogarciaperez00@gmail.com>
2025-02-20 14:19:54 +0100

    Ej2 completo

--- ej2.py
+++ ej2.py
@@ -1,3 +1,5 @@
+from ayuda import printMatriz
+
 fila = 3
 columna = 4
 
@@ -9,3 +11,17 @@ for nFilas in range(fila):
         valor = int(input(f"Introduce los valores para{nFilas+1},{nColumnas+1} "))
         matriz[nFilas].append(valor)
 
+sumaFilas = []
+for i in range (fila):
+    valor = 0
+    for j in range(columna):
+        valor += matriz[i][j]
+    sumaFilas.append(valor)
+
+valorMaximo = []
+máximo = max(sumaFilas)
+valorMaximo.append(máximo)
+
+print(printMatriz(matriz))
+print(sumaFilas)
+print(f"El resultado mayor es {valorMaximo}")
