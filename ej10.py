41c87ea25794746dc6281a3aa647a371d7ecb839
rodgar00 <rodrigogarciaperez00@gmail.com>
2025-03-03 12:22:24 +0100

    Ejercicio 10 a medias

---
+++ ej10.py
@@ -0,0 +1,17 @@
+almacenes = 2
+pieza = 3
+costes = []
+tablaM3N2 = [
+    [31, 42, 64],
+    [50, 101, 194],
+    [50, 101, 194],
+    [19.61, 23, 86.4]
+]
+sumaTotal = sum(tablaM3N2[0] + tablaM3N2[1])
+print(f"Este es el valor general: {sumaTotal}")
+valorTotalPieza1 = sum(tablaM3N2[fila][0] for fila in range(almacenes))
+print(f"Este es el valor de la pieza 1: {valorTotalPieza1}")
+valorTotalPieza2 = sum(tablaM3N2[fila][1] for fila in range(almacenes))
+print(f"Este es el valor de la pieza 2: {valorTotalPieza2}")
+valorTotalPieza3 = sum(tablaM3N2[fila][2] for fila in range(almacenes))
+print(f"Este es el valor de la pieza 3: {valorTotalPieza3}")
